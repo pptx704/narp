6f12f2b404075e353998475b2fd733b56c44d6d9
pptx704 <rafeedm.bhuiyan@gmail.com>
2023-06-21 04:15:35 +0300

    Added dependencies

--- narpy/server.py
+++ narpy/server.py
@@ -79,6 +79,7 @@ class Server:
             return action_space
         except ValueError:
             return -1
+        
     def fetch_state_space(self) -> int:
         """
         Fetches the action space from the client.
